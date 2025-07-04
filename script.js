3ec649c41eb508f6ac77191ca673c242a378b4fb
Margot Gouriou <margot.gouriou@gmail.com>
2025-07-04 11:19:29 +0200

    function addquote

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
+// etape 4
+
 function recupererValeurs() {
-    // Récupérer les éléments du formulaire par leur ID
-    var text = document.getElementById("citations-input").value;
-    var author = document.getElementById("auteur-input").value;
+  // Récupérer les éléments du formulaire par leur ID
+  var text = document.getElementById("citations-input").value;
+  var author = document.getElementById("auteur-input").value;
 
-    // Afficher les valeurs récupérées
-    console.log("Nom :", citations-input);
-    console.log("Email :", auteur-input);}
+  // Afficher les valeurs récupérées
+  console.log("Citation :", citations - input);
+  console.log("Auteur :", auteur - input);
+}
 
-    // Vous pouvez maintenant utiliser ces valeurs comme vous le souhaitez
-    
+// Vous pouvez maintenant utiliser ces valeurs comme vous le souhaitez
+// marche pas :(
+
+// etape 5
+
+function addquote(quote, author) {
+let citation = document.createElement('text');
+let auteur = document.createElement('author');
+
+
+}
